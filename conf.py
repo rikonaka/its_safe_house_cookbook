bc0af5094bf17166d737de4440be4d51a9da4a88
Riko Desktop <xxy1836@gmail.com>
2020-02-10 19:10:04 +0800

    fix markdown table

--- conf.py
+++ conf.py
@@ -47,6 +47,7 @@ release = 'v0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx-markdown-tables",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
